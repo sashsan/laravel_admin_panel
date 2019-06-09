b616c14d7966c368ceac31ea81b3430686ab0148
SashaSan <s@sashasan.com>
2019-06-09 05:38:21 +0300

    Create start ProductController

---
+++ app/Repositories/Admin/ProductRepository.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Sasha San
+     * Date: 09.06.2019
+     * Time: 4:17
+     */
+
+    namespace App\Repositories\Admin;
+
+
+    class ProductRepository
+    {
+
+    }
---
+++ resources/views/blog/admin/product/create.blade.php
@@ -0,0 +1,7 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Sasha San
+     * Date: 09.06.2019
+     * Time: 4:50
+     */
